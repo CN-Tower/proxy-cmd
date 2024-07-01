71cef405ddf8055182478e00e037eec36dd4bedb
cntower <shentao@bilibili.com>
2024-07-01 14:51:42 +0800

    feat: windows alias.bat

--- bin/alias.bat
+++ bin/alias.bat
@@ -1,3 +1,3 @@
 @echo off
-doskey proxy-on=set HTTP_PROXY=%PROXY_URL%
-doskey proxy-off=set HTTP_PROXY=
+doskey proxy-on=set HTTP_PROXY=%PROXY_URL% $T HTTPS_PROXY=%PROXY_URL%
+doskey proxy-off=set HTTP_PROXY= $T HTTPS_PROXY=
